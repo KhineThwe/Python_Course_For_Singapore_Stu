8f94142fd32c5989f4fe127968e6d220b598516d
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2023-03-14 01:26:02 -1200

    Create 1san.lessonDictionary1.py

---
+++ 1san.lessonDictionary1.py
@@ -0,0 +1,8 @@
+user = {
+    'name':'Khine',
+    'gender':'female'
+}
+print(user['gender'])
+print(user.get('name'))
+print(user.get('nationality'))#None
+print(user.get('nationality','myanmar'))
